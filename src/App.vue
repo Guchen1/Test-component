e50f71b66d791c86388ec833a038c835e15d3b3e
Guchen <guchen3@media.com>
2023-08-03 15:37:22 +0800

    Art fail

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-    <Button :size="'small'" >你好</Button><Button>你好</Button><Button :size="'large'">你好</Button>
+    <Button  >你好</Button>
+    <Button type="info" >你好</Button>
+    <Button  type="success" >你好</Button>
+    <Button  type="warning" >你好</Button>
+    <Button  type="danger" >你好</Button>
 </template>
 <script setup lang="ts">
 import { Button } from './components';
