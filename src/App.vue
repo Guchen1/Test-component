e6ad1f9bb03e3d88286ed09dfc32d4b779ce8363
Guchen <guchen3@media.com>
2023-08-03 16:44:28 +0800

    Change color

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-    <Button  >你好</Button>
-    <Button type="info" >你好</Button>
-    <Button  type="success" >你好</Button>
-    <Button  type="warning" >你好</Button>
-    <Button  type="danger" >你好</Button>
+    <Button style="margin-right:5px">你好</Button>
+    <Button style="margin-right:5px" type="info" >你好</Button>
+    <Button style="margin-right:5px" type="success" >你好</Button>
+    <Button style="margin-right:5px" type="warning" >你好</Button>
+    <Button style="margin-right:5px" type="danger" >你好</Button>
 </template>
 <script setup lang="ts">
 import { Button } from './components';
