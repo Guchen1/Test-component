dcfeeaa842d86a3ce1aa10782cd5ed5f850585ff
Guchen <guchen3@media.com>
2023-08-03 17:11:33 +0800

    Delete node_modules

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,13 @@
     <Button style="margin-right:5px" type="success" >你好</Button>
     <Button style="margin-right:5px" type="warning" >你好</Button>
     <Button style="margin-right:5px" type="danger" >你好</Button>
+    <fluent-button appearance="accent">Click Me</fluent-button>
 </template>
 <script setup lang="ts">
 import { Button } from './components';
+import { provideFluentDesignSystem, fluentCard, fluentButton } from '@fluentui/web-components';
+provideFluentDesignSystem().register(fluentCard(), fluentButton());
+//让组件可以在模板中使用
+
 
 </script>
